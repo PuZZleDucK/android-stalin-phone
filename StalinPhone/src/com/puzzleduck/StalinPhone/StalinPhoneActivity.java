b7c05e5a10bf7b67b229965339d47633b4711628
puzzleduck <puzzleduck@bcfa9fb5-7d3f-7c44-7ba0-6beb766bbbad>
2011-10-31 09:03:56 +0000

    god knows... i did this days ago... fingers crossed :]

--- StalinPhone/src/com/puzzleduck/StalinPhone/StalinPhoneActivity.java
+++ StalinPhone/src/com/puzzleduck/StalinPhone/StalinPhoneActivity.java
@@ -1,15 +1,128 @@
 package com.puzzleduck.StalinPhone;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.app.PendingIntent.CanceledException;
+import android.content.Intent;
 import android.os.Bundle;
-
-public class StalinPhoneActivity extends Activity {
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.AdapterView.OnItemClickListener;
+ 
+public class StalinPhoneActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+         
+//        Button startButton = (Button) findViewById(R.id.start_stalin_trans_service);
+  
+//        Button button = (Button)findViewById(R.id.start_stalin_trans_service);
+        Log.d("StalinPhone ::: ", "buttons selected");
+//        button.setOnClickListener((OnClickListener) this);//BUG!!!
+        //or ((AutoCompleteTextView) findViewById(R.id.autoCompleteRegion)).setOnItemClickListener(aRegionClickListener);
+//        button.setOnKeyListener(mStartTranslateListener);
+        findViewById(R.id.start_stalin_trans_service).setOnClickListener( (OnClickListener) this);
+//        Log.d("StalinPhone ::: ", "button 1 set up");
+        findViewById(R.id.stop_stalin_trans_service).setOnClickListener( (OnClickListener) this);
+        Log.d("StalinPhone ::: ", "trans buttons set up");
+        findViewById(R.id.start_stalin_rec_service).setOnClickListener( (OnClickListener) this);
+        findViewById(R.id.stop_stalin_rec_service).setOnClickListener( (OnClickListener) this);
+        Log.d("StalinPhone ::: ", "rec buttons set up");
+//        Button button = (Button)findViewById(R.id.start_stalin_trans_service);
+        // Register the onClick listener with the implementation above
+//        button.setOnClickListener(startStalinClickListener);
+
+        
+        
+        
+//        button = (Button)findViewById(R.id.stopTransService);
+//        button.setOnClickListener((android.view.View.OnClickListener) mStopTranslateListener);
+    
+    
+    
+    } 
+
+    private PendingIntent mTransRequestSender;
+
+	@Override
+	public void onClick(View v) {
+        Log.d("StalinPhone ::: ", "button clicked: id" + v.getId());
+      //start/kill service
+
+        if(v.getId() == R.id.start_stalin_rec_service)
+        {
+//            Log.d("StalinPhone ::: ", "button clicked: start rec");
+
+            Log.d("StalinPhone ::: ", "creating rec intent");
+
+            mTransRequestSender = PendingIntent.getService(StalinPhoneActivity.this,
+            		0, new Intent(StalinPhoneActivity.this, StalinRecService.class), 0);
+
+            Log.d("StalinPhone ::: ", "rec intent created");
+            try {
+    			mTransRequestSender.send();
+    		} catch (CanceledException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}        
+        }
+        if(v.getId() == R.id.stop_stalin_rec_service)
+        {
+//            Log.d("StalinPhone ::: ", "button clicked: stop rec");
+            Log.d("StalinPhone ::: ", "creating stop-rec intent");
+
+            mTransRequestSender = PendingIntent.getService(StalinPhoneActivity.this,
+            		0, new Intent(StalinPhoneActivity.this, StalinStopRecService.class), 0);
+
+            Log.d("StalinPhone ::: ", "stop-rec intent created");
+            try {
+    			mTransRequestSender.send();
+    		} catch (CanceledException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+        }
+        if(v.getId() == R.id.start_stalin_trans_service)
+        {
+            Log.d("StalinPhone ::: ", "button clicked: start t");
+//            Log.d("StalinPhone ::: ", "creating intent");
+//
+//            mTransRequestSender = PendingIntent.getService(StalinPhoneActivity.this,
+//            		0, new Intent(StalinPhoneActivity.this, StalinTransService.class), 0);
+//
+//            Log.d("StalinPhone ::: ", "intent created");
+//            try {
+//    			mTransRequestSender.send();
+//    		} catch (CanceledException e) {
+//    			// TODO Auto-generated catch block
+//    			e.printStackTrace();
+//    		}
+        }
+        if(v.getId() == R.id.stop_stalin_trans_service)
+        {
+            Log.d("StalinPhone ::: ", "button clicked: stop t");
+//            Log.d("StalinPhone ::: ", "creating intent");
+//
+//            mTransRequestSender = PendingIntent.getService(StalinPhoneActivity.this,
+//            		0, new Intent(StalinPhoneActivity.this, StalinTransService.class), 0);
+//
+//            Log.d("StalinPhone ::: ", "intent created");
+//            try {
+//    			mTransRequestSender.send();
+//    		} catch (CanceledException e) {
+//    			// TODO Auto-generated catch block
+//    			e.printStackTrace();
+//    		}
+        }
+
+
+	}
 }
 
 // receiving Intent { act=android.intent.action.NEW_OUTGOING_CALL (has extras) }
--- StalinPhone/src/com/puzzleduck/StalinPhone/StalinReceiver.java
+++ StalinPhone/src/com/puzzleduck/StalinPhone/StalinReceiver.java
@@ -3,6 +3,8 @@ package com.puzzleduck.StalinPhone;
 import java.io.File;
 import java.io.IOException;
 
+import android.app.PendingIntent;
+import android.app.PendingIntent.CanceledException;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.ContentValues;
@@ -15,12 +17,16 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class StalinReceiver extends BroadcastReceiver {
+public class StalinReceiver extends BroadcastReceiver{
+ 
 
-	private MediaRecorder recorder = new MediaRecorder();
+    private PendingIntent mTransRequestSender;
+    public static String state;
 	@Override
 	public void onReceive(Context context, Intent intent) {
+
 //		Bundle extraData = intent.getExtras();
 //		if(extraData != null)
 //		{
@@ -37,11 +43,13 @@ public class StalinReceiver extends BroadcastReceiver {
 		//props: http://www.vogella.de/articles/AndroidServices/article.html
 		Bundle extras = intent.getExtras();
 		if (extras != null) {
-			String state = extras.getString(TelephonyManager.EXTRA_STATE);
+			state = extras.getString(TelephonyManager.EXTRA_STATE);
 			Log.d("DEBUG", "STALINphone ::: State: " + state);
 			
 			if (state != null) 
 			{
+
+
 				if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
 					Log.d("DEBUG", "STALINphone ::: Ringing ");
 					//				String phoneNumber = extras
@@ -54,99 +62,70 @@ public class StalinReceiver extends BroadcastReceiver {
 					//this all needs to go in a service!!! BIG TIME
 					//this receiver is never terminating... properly
 					//therefore the new intent kills this one before stop and release are called
-					
-					//http://www.androidtutorials.org/
-					String mediaState = android.os.Environment
-							.getExternalStorageState();
-					if (!mediaState
-							.equals(android.os.Environment.MEDIA_MOUNTED)) {
-						try {
-							throw new IOException(
-									"STALINphone ::: SD Card is not mounted. It is "
-											+ mediaState + ".");
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
+				
 
-					// make sure the directory we plan to store the recording in exists
-					//				  File directory = new File(path).getParentFile();
-//					File myNewFolder = new File(
-//							Environment.getExternalStorageDirectory()
-//									+ "StalinPhone");
-//					myNewFolder.mkdir();
+			
+			//call recievers for processing/recording
+				//NOW USING INTENTS
+		        Log.d("StalinPhone ::: ", "creating recording intent");
 
-					//					Log.d("DEBUG", "STALINphone ::: proposed-directory: " + myNewFolder + "/rec01.3gp");
+		        mTransRequestSender = PendingIntent.getService(context,
+		        		0, new Intent(context, StalinRecService.class), 0);
 
-					File directory = new File(Environment.getExternalStorageDirectory() + "/StalinPhone/rec"+System.currentTimeMillis()+".mp4");//.getParentFile();
-					
-					//					Log.d("DEBUG", "STALINphone ::: directory: " + directory.getAbsolutePath());
-					//				  if (!directory.exists() && !directory.mkdirs()) {
-					//					  try {
-					//						throw new IOException("STALINphone ::: Path to file could not be created.");
-					//					} catch (IOException e) {
-					//						// TODO Auto-generated catch block
-					//						e.printStackTrace();
-					//					}
-					//				  }
-					Log.d("DEBUG", "STALINphone ::: directory: " + directory.getPath());
-					//					Log.d("DEBUG", "STALINphone ::: audio sources: " + MediaRecorder.getAudioSourceMax());
-					//					  recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
-					recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
-					Log.d("DEBUG",
-							"STALINphone ::: audio source set - MAX AMP: "
-									+ recorder.getMaxAmplitude());
-					recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-					Log.d("DEBUG", "STALINphone ::: audio format set: ");
-					recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-					Log.d("DEBUG", "STALINphone ::: audio encoder set: ");
-
-					//					ContentResolver contentResolver = new ContentResolver();
-					//					ContentValues values = new ContentValues(3);
-					//					values.put(MediaStore.MediaColumns.TITLE, SOME_NAME_HERE);
-
-					//					Uri base = MediaStore.Audio.Media.INTERNAL_CONTENT_URI;
-					//					Uri newUri = contentResolver.insert(base, values);
-					//					String path = contentResolver.getDataFilePath(newUri);
-					//					ContentResolver.
-
-					recorder.setOutputFile( directory.getPath()  );
-					Log.d("DEBUG", "STALINphone ::: audio file set: ");
-
-					try {
-						recorder.prepare();
-						Log.d("DEBUG", "STALINphone ::: prepare: ");
-					} catch (IllegalStateException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					recorder.start();
-					Log.d("DEBUG", "STALINphone ::: started: ");
-
-					//				String phoneNumber = extras
-					//						.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+		        Log.d("StalinPhone ::: ", "recording intent created");
+		        try {
+					mTransRequestSender.send();
+				} catch (CanceledException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				//INTENT
 
 				}//EXTRA_STATE_OFFHOOK
+
 				if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
 					//stop recording ... start dictation service
 					//then when complete dictation service will start "review" UI
 
+					
+					
+//					StalinRecService.this.notify();
+//					
+//					//NOW USING INTENTS
+//			        Log.d("StalinPhone ::: ", "creating stop-recording intent");
+//
+//			        mTransRequestSender = PendingIntent.getService(context,
+//			        		0, new Intent(context, StalinStopRecService.class), 0);
+//
+//			        Log.d("StalinPhone ::: ", "stop-recording intent created");
+//			        try {
+//						mTransRequestSender.send();
+//					} catch (CanceledException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//					//INTENT
+					
+					
 					//check that we are still running!!
 					//not sure i can???
 
-					recorder.stop();
-					Log.d("DEBUG", "STALINphone ::: Idle - STOP REC ");
-					recorder.release();
-					Log.d("DEBUG", "STALINphone ::: Idle - RELEASE REC ");
 
 					//				String phoneNumber = extras
 					//						.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
 
 				}//EXTRA_STATE_IDLE
+
+	        	
+	        	
+	        	
+	        	
+	        	
+	        	
+	            
+				
+				
+			
 			}//if state==null
 		}
 
