d22ccd5662efa67e65ee179af4fbbc37c96d724c
puzzleduck <puzzleduck@bcfa9fb5-7d3f-7c44-7ba0-6beb766bbbad>
2011-10-31 10:09:42 +0000

    removed old icon

--- trunk/StalinPhone/src/com/puzzleduck/StalinPhone/StalinPhoneActivity.java
+++ trunk/StalinPhone/src/com/puzzleduck/StalinPhone/StalinPhoneActivity.java
@@ -20,32 +20,14 @@ public class StalinPhoneActivity extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
          
-//        Button startButton = (Button) findViewById(R.id.start_stalin_trans_service);
-  
-//        Button button = (Button)findViewById(R.id.start_stalin_trans_service);
         Log.d("StalinPhone ::: ", "buttons selected");
-//        button.setOnClickListener((OnClickListener) this);//BUG!!!
-        //or ((AutoCompleteTextView) findViewById(R.id.autoCompleteRegion)).setOnItemClickListener(aRegionClickListener);
-//        button.setOnKeyListener(mStartTranslateListener);
         findViewById(R.id.start_stalin_trans_service).setOnClickListener( (OnClickListener) this);
-//        Log.d("StalinPhone ::: ", "button 1 set up");
         findViewById(R.id.stop_stalin_trans_service).setOnClickListener( (OnClickListener) this);
         Log.d("StalinPhone ::: ", "trans buttons set up");
         findViewById(R.id.start_stalin_rec_service).setOnClickListener( (OnClickListener) this);
         findViewById(R.id.stop_stalin_rec_service).setOnClickListener( (OnClickListener) this);
         Log.d("StalinPhone ::: ", "rec buttons set up");
-//        Button button = (Button)findViewById(R.id.start_stalin_trans_service);
-        // Register the onClick listener with the implementation above
-//        button.setOnClickListener(startStalinClickListener);
 
-        
-        
-        
-//        button = (Button)findViewById(R.id.stopTransService);
-//        button.setOnClickListener((android.view.View.OnClickListener) mStopTranslateListener);
-    
-    
-    
     } 
 
     private PendingIntent mTransRequestSender;
@@ -53,18 +35,14 @@ public class StalinPhoneActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
         Log.d("StalinPhone ::: ", "button clicked: id" + v.getId());
-      //start/kill service
-
+      //work out action needed and start/kill service
         if(v.getId() == R.id.start_stalin_rec_service)
         {
-//            Log.d("StalinPhone ::: ", "button clicked: start rec");
-
-            Log.d("StalinPhone ::: ", "creating rec intent");
-
+            Log.d("StalinPhone ::: ", "creating REC intent");
             mTransRequestSender = PendingIntent.getService(StalinPhoneActivity.this,
             		0, new Intent(StalinPhoneActivity.this, StalinRecService.class), 0);
 
-            Log.d("StalinPhone ::: ", "rec intent created");
+            Log.d("StalinPhone ::: ", "REC intent created");
             try {
     			mTransRequestSender.send();
     		} catch (CanceledException e) {
@@ -74,13 +52,11 @@ public class StalinPhoneActivity extends Activity implements OnClickListener {
         }
         if(v.getId() == R.id.stop_stalin_rec_service)
         {
-//            Log.d("StalinPhone ::: ", "button clicked: stop rec");
-            Log.d("StalinPhone ::: ", "creating stop-rec intent");
-
+            Log.d("StalinPhone ::: ", "creating stop-REC intent");
             mTransRequestSender = PendingIntent.getService(StalinPhoneActivity.this,
             		0, new Intent(StalinPhoneActivity.this, StalinStopRecService.class), 0);
 
-            Log.d("StalinPhone ::: ", "stop-rec intent created");
+            Log.d("StalinPhone ::: ", "stop-REC intent created");
             try {
     			mTransRequestSender.send();
     		} catch (CanceledException e) {
@@ -92,37 +68,13 @@ public class StalinPhoneActivity extends Activity implements OnClickListener {
         {
             Log.d("StalinPhone ::: ", "button clicked: start t");
 //            Log.d("StalinPhone ::: ", "creating intent");
-//
-//            mTransRequestSender = PendingIntent.getService(StalinPhoneActivity.this,
-//            		0, new Intent(StalinPhoneActivity.this, StalinTransService.class), 0);
-//
-//            Log.d("StalinPhone ::: ", "intent created");
-//            try {
-//    			mTransRequestSender.send();
-//    		} catch (CanceledException e) {
-//    			// TODO Auto-generated catch block
-//    			e.printStackTrace();
-//    		}
         }
         if(v.getId() == R.id.stop_stalin_trans_service)
         {
             Log.d("StalinPhone ::: ", "button clicked: stop t");
 //            Log.d("StalinPhone ::: ", "creating intent");
-//
-//            mTransRequestSender = PendingIntent.getService(StalinPhoneActivity.this,
-//            		0, new Intent(StalinPhoneActivity.this, StalinTransService.class), 0);
-//
-//            Log.d("StalinPhone ::: ", "intent created");
-//            try {
-//    			mTransRequestSender.send();
-//    		} catch (CanceledException e) {
-//    			// TODO Auto-generated catch block
-//    			e.printStackTrace();
 //    		}
         }
 
-
-	}
-}
-
-// receiving Intent { act=android.intent.action.NEW_OUTGOING_CALL (has extras) }
+	}//onClick
+}//class
